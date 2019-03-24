26e57cf7e68471216e629dfea602aa7ec233afb5
Nora Coates <31967440+NoraWC@users.noreply.github.com>
2019-03-24 16:03:37 -0700

    Update CalcProject.js

--- CalcProject.js
+++ CalcProject.js
@@ -2,8 +2,10 @@
 function calculate() {
     var inp = document.getElementById("funcIn").value;
     for(i=0; i<inp.length; i++) {
-        if(inp[i] = "^") {
-            var here = inp[i-2,i+1];
+        if(inp[i] = "x^") {
+            var exp = inp[i+1];
+            var temp = exp+"x";
+            inp = inp[0,i-1]+temp+inp[i+1,inp.length];
             console.log(here);
         }
     }
