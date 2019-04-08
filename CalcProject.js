96bce512225f966628352e368f9c9fab8994f321
Nora Coates <31967440+NoraWC@users.noreply.github.com>
2019-04-08 16:40:59 -0700

    Update CalcProject.js

--- CalcProject.js
+++ CalcProject.js
@@ -4,16 +4,18 @@ console.log(str_inp);
 function calculate(str_inp) {
     //var inp = document.getElementById("funcIn").value;
     str_inp = "x^3";
-    for(i = 0; i < str_inp.length; i++) {
+    l = str_inp.length;
+    for(i = 0; i < l; i++) {
         console.log(str_inp);
         console.log(str_inp.length);
+        console.log(l);
         if(str_inp[i] = "^") {
             console.log(i);
             var exponent = str_inp[i];
             console.log(exponent);
             var temporary = exponent+"x";
             console.log(temporary);
-            str_inp = str_inp[0,i-1]+temporary+str_inp[i+1,str_inp.length];
+            str_inp = str_inp[0,i-1]+temporary+str_inp[i+1, l];
             console.log(str_inp);
         }
     }
