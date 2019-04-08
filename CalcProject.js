a5a51cba5c728454873ceec1b87224bd999edd77
Nora Coates <31967440+NoraWC@users.noreply.github.com>
2019-04-08 16:06:15 -0700

    Update CalcProject.js

--- CalcProject.js
+++ CalcProject.js
@@ -2,6 +2,7 @@
 inp = "x^3";
 function calculate(inp) {
     //var inp = document.getElementById("funcIn").value;
+    inp = "x^3";
     for(i=0; i<inp.length(); i++) {
         if(inp[i] = "^") {
             var exp = inp[i+1];
